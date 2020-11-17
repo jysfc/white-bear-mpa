4f2691dff4b6468386aca0f68f5b60f381fcd027
jysfc <63255178+jysfc@users.noreply.github.com>
2020-11-17 00:13:55 -0800

    added review done

--- src/components/pages/ReviewDone.js
+++ src/components/pages/ReviewDone.js
@@ -1,6 +1,20 @@
 import React from "react";
 import AppTemplate from "../ui/AppTemplate";
+import { Link } from "react-router-dom";
 
 export default function ReviewDone() {
-   return <AppTemplate>ReviewDone</AppTemplate>;
+   return (
+      <AppTemplate>
+         {/* <!-- Comment above Card --> */}
+         <h4 class="text-center lead text-muted my-2">Out of cards</h4>
+
+         {/* <!-- buttons --> */}
+         <Link to="/review-answer" class="btn btn-link" id="back-to-answer">
+            Previous card
+         </Link>
+         <Link to="/review-cue" class="btn btn-outline-primary float-right">
+            Get more cards
+         </Link>
+      </AppTemplate>
+   );
 }
