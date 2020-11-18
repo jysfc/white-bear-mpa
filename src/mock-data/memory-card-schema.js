00fcc32c35d25df52eb39787d808aa181351884e
jysfc <63255178+jysfc@users.noreply.github.com>
2020-11-17 16:04:55 -0800

    added notes to mock data

--- src/mock-data/memory-card-schema.js
+++ src/mock-data/memory-card-schema.js
@@ -1,9 +1,10 @@
 const memoryCardSchema = {
-   id: String,
-   imagery: String,
-   answer: String,
-   userId: String,
-   createdAt: Number,
+   id: String, //hex#
+   imagery: String, //top part of card
+   answer: String, //bottom part of card
+   userId: String, //hex UUID of user
+   createdAt: Number, //timestamp
+   //business logic used to filter
    nextAttemptAt: Number,
    lastAttemptAt: Number,
    totalSuccessfulAttempts: Number,
