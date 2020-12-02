b25c9bd4d921583dc2627abac3f265fe9b8766e0
jysfc <63255178+jysfc@users.noreply.github.com>
2020-12-01 23:28:29 -0800

    added axios, made mocky

--- src/components/pages/ReviewCue.js
+++ src/components/pages/ReviewCue.js
@@ -2,30 +2,52 @@ import React from "react";
 import AppTemplate from "../ui/AppTemplate";
 import { Link } from "react-router-dom";
 import memoryCards from "../../mock-data/memory-cards";
+import axios from "axios";
 
 const memoryCard = memoryCards[3];
 
-export default function ReviewCue() {
-   return (
-      <AppTemplate>
-         <div className="mb-5"></div>
-         {/* <!-- Card --> */}
-         <div className="card">
-            <div className="card-body bg-primary lead mb-5">
-               {memoryCard.imagery}
-            </div>
-         </div>
+class ReviewCue extends React.Component {
+   constructor(props) {
+      super(props);
+      axios
+         .get("https://run.mocky.io/v3/c88813b7-43f7-4dfb-8a80-fccec1f99bee")
+         .then(function (response) {
+            // handle success
+            console.log(response);
+         })
+         .catch(function (error) {
+            // handle error
+            console.log(error);
+         });
+   }
 
-         {/* <!-- buttons --> */}
-         <Link to="/review-answer" className="btn btn-link" id="back-to-answer">
-            Previous card
-         </Link>
-         <Link
-            to="/review-answer"
-            className="btn btn-outline-primary float-right"
-         >
-            Show answer
-         </Link>
-      </AppTemplate>
-   );
+   render() {
+      return (
+         <AppTemplate>
+            <div className="mb-5"></div>
+            {/* <!-- Card --> */}
+            <div className="card">
+               <div className="card-body bg-primary lead mb-5">
+                  {memoryCard.imagery}
+               </div>
+            </div>
+
+            {/* <!-- buttons --> */}
+            <Link
+               to="/review-answer"
+               className="btn btn-link"
+               id="back-to-answer"
+            >
+               Previous card
+            </Link>
+            <Link
+               to="/review-answer"
+               className="btn btn-outline-primary float-right"
+            >
+               Show answer
+            </Link>
+         </AppTemplate>
+      );
+   }
 }
+export default ReviewCue;
